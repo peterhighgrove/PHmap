50ed5436ee6d12f770ea3943826d1ec3ae1b741d
PeterH <37709581+peterhighgrove@users.noreply.github.com>
2024-08-15 21:46:31 +0200

    new file:   hs_err_pid9864.log     Error when running mkgmap for unknown reasen. --max-jobs seams to solv it
    	modified:   scripts/runMKGmap.bat
        added chcp 65001 to be able to handle swe char i dir in bat file
        added copy to 1drv

--- scripts/runMKGmap.bat
+++ scripts/runMKGmap.bat
@@ -1,9 +1,8 @@
+chcp 65001
 ::Sweden
-::java -Xmx2000m -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\splitter\splitter.jar" --max-nodes=1550000 --output-dir="C:\Users\peter\Documents\Maps Garmin\OSMsplitted\SE" --mapid=63210001 --description=SE_240804_OSM_tile "C:\Users\peter\Documents\Maps Garmin\OSMlatest\sweden-latest.osm.pbf"
+Java -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\mkgmap.jar" --max-jobs=8 --generate-sea=multipolygon --country-name=Sweden --country-abbr=SE --family-id=4000 --family-name=PHmap --product-id=1000 --draw-priority=21 --code-page=1252 --style-file="C:\Users\peter\Documents\Maps Garmin\PHmap\styles\all" --output-dir="C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps" --gmapsupp -c "C:\Users\peter\Documents\Maps Garmin\OSMsplitted\SE\template.args" --description=SE_PHmap_240804-phTyp25 "C:\Users\peter\Documents\Maps Garmin\PHmap\typ\phTyp25.typ"
 
-::Java -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\mkgmap.jar" --generate-sea=multipolygon --country-name=Sweden --country-abbr=SE --family-id=4000 --family-name=PHmap --product-id=1000 --draw-priority=21 --code-page=1252 --style-file="C:\Users\peter\Documents\Maps Garmin\PHmap\styles\all" --output-dir="C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps" --gmapsupp -c "C:\Users\peter\Documents\Maps Garmin\OSMsplitted\SE\template.args" --description=SE_PHmap_240804-phTyp25 "C:\Users\peter\Documents\Maps Garmin\PHmap\typ\phTyp25.typ"
-
-::move /Y "C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps\gmapsupp.img" "C:\Users\peter\Documents\Maps Garmin\PHmaps\SE_PHmap_latest_phTyp25.img"
+move /Y "C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps\gmapsupp.img" "C:\Users\peter\Documents\Maps Garmin\PHmaps\SE_PHmap_latest_phTyp25.img"
 copy "C:\Users\peter\Documents\Maps Garmin\PHmaps\SE_PHmap_latest_phTyp25.img" "C:\Users\peter\OneDrive\Dokument Peter OneDrive\Träning\Maps Garmin IMG\PHmaps"
 
 ::France Languedoc
@@ -12,6 +11,7 @@ copy "C:\Users\peter\Documents\Maps Garmin\PHmaps\SE_PHmap_latest_phTyp25.img" "
 ::java -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\mkgmap.jar" --generate-sea=multipolygon --country-name=France --country-abbr=FR --region-name=Languedoc --region-abbr=ld --family-id=4000 --family-name=PHmap --product-id=2000 --draw-priority=21 --code-page=1252 --style-file="C:\Users\peter\Documents\Maps Garmin\PHmap\styles\all" --output-dir="C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps" --gmapsupp -c "C:\Users\peter\Documents\Maps Garmin\OSMsplitted\FRld\template.args" --description=FRld_PHmap_240804-phTyp25 "C:\Users\peter\Documents\Maps Garmin\PHmap\typ\phTyp25.typ"
 
 ::Move /Y "C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps\gmapsupp.img" "C:\Users\peter\Documents\Maps Garmin\PHmaps\FRld_PHmap_latest_phTyp25.img"
+copy "C:\Users\peter\Documents\Maps Garmin\PHmaps\FRld_PHmap_latest_phTyp25.img" "C:\Users\peter\OneDrive\Dokument Peter OneDrive\Träning\Maps Garmin IMG\PHmaps"
 
 ::France Rhone-Alpes
 ::java -Xmx2000m -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\splitter\splitter.jar" --output-dir="C:\Users\peter\Documents\Maps Garmin\OSMsplitted\FRra" --mapid=63230001 --description=FRra_240805_OSM_tile "C:\Users\peter\Documents\Maps Garmin\OSMlatest\rhone-alpes-latest.osm.pbf"
@@ -19,4 +19,5 @@ copy "C:\Users\peter\Documents\Maps Garmin\PHmaps\SE_PHmap_latest_phTyp25.img" "
 ::java -jar "C:\Users\peter\Documents\Maps Garmin\MKGmap\mkgmap.jar" --generate-sea=multipolygon --country-name=France --country-abbr=FR --region-name=Rhone-Alpes --region-abbr=ra --family-id=4000 --family-name=PHmap --product-id=3000 --draw-priority=21 --code-page=1252 --style-file="C:\Users\peter\Documents\Maps Garmin\PHmap\styles\all" --output-dir="C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps" --gmapsupp -c "C:\Users\peter\Documents\Maps Garmin\OSMsplitted\Frra\template.args" --description=FRra_PHmap_240805-phTyp25 "C:\Users\peter\Documents\Maps Garmin\PHmap\typ\phTyp25.typ"
 
 ::Move /Y "C:\Users\peter\Documents\Maps Garmin\OSM_Gmaps\gmapsupp.img" "C:\Users\peter\Documents\Maps Garmin\PHmaps\FRra_PHmap_latest_phTyp25.img"
+copy "C:\Users\peter\Documents\Maps Garmin\PHmaps\FRra_PHmap_latest_phTyp25.img" "C:\Users\peter\OneDrive\Dokument Peter OneDrive\Träning\Maps Garmin IMG\PHmaps"
 
